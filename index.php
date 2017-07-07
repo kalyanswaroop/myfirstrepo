1528419708afeaefc0203e41f9709ab266e3bf7f
kalyan's <kalyan's@192.168.1.9>
2017-07-07 16:46:32 +0530

    Pushing my first index.php file

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+	echo "Hello World";
+	echo "<br> My first PHP program on eclipse";	
+
+	class Demo{
+		
+		public function one() {
+			return 1;
+		}
+		
+		public function two() {
+			return "A word";
+		}
+		
+	}
+	$obj=new Demo();
+	echo "\nOne function output: ".$obj->one();
+	echo "\nTwo function output: ".$obj->two();
+?>
